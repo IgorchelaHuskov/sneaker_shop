160f5f847c4a829e1ee7c073ade492b27e4d8cd1
Igor <guskov8527gmail.com>
2023-09-06 19:58:30 +0300

    Добавлен скрипт установки пакетов

---
+++ setup.py
@@ -0,0 +1,58 @@
+"""Скрипт установки приложения admin-jwt-authentication."""
+from pathlib import Path
+from setuptools import setup, find_packages
+
+DESCRIPTION = (
+    "Flask API with Flask-RESTx, SQLAlchemy, pytest, flake8, "
+    "tox configured"
+)
+APP_ROOT = Path(__file__).parent
+README = (APP_ROOT / "README.md").read_text()
+AUTHOR = "Igor Guskov"
+AUTHOR_EMAIL = "guskov8527@gmail.com"
+INSTALL_REQUIRES = [
+    "Flask",
+    "Flask-Bcrypt",
+    "Flask-Cors",
+    "Flask-Migrate",
+    "flask-restx",
+    "Flask-SQLAlchemy",
+    "PyJWT",
+    "python-dateutil",
+    "python-dotenv",
+    "requests",
+    "urllib3",
+    "werkzeug==2.3.7 ",
+
+]
+EXTRAS_REQUIRE = {
+    "dev": [
+        "flake8",
+        "pre-commit",
+        "pydocstyle",
+        "pytest",
+        "pytest-clarity",
+        "pytest-dotenv",
+        "pytest-flake8",
+        "pytest-flask",
+        "tox",
+    ]
+}
+
+setup(
+    name="admin-jwt-authentication",
+    description=DESCRIPTION,
+    long_description=README,
+    long_description_content_type="text/markdown",
+    version="0.1",
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    maintainer=AUTHOR,
+    maintainer_email=AUTHOR_EMAIL,
+    url="https://github.com/IgorchelaHuskov/admin-jwt-authentication",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.11",
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+)
