13483172a57313f784c36a3b3f10b7ada423b609
Igor <guskov8527gmail.com>
2023-09-12 13:19:29 +0300

    Импорт User model для того чтобы flask migrate обноружил model User

--- run.py
+++ run.py
@@ -2,10 +2,11 @@
 import os
 
 from jwt_authentication import create_app, db
+from jwt_authentication.models.user import User
 
 app = create_app(os.getenv("FLASK_ENV", "development"))
 
 
 @app.shell_context_processor
 def shell():
-    return {"db" : db}
+    return {"db" : db, "User": User}
