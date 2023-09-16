f4f927c351c133b7f827504c5c1a5a1f3af5dbf5
Igor <guskov8527gmail.com>
2023-09-16 12:58:02 +0300

    Изменения модели

--- src/jwt_authentication/models/user.py
+++ src/jwt_authentication/models/user.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.hybrid import hybrid_property
 
 from jwt_authentication import db, bcrypt
 from jwt_authentication.util.datetime_util import (
-    uts_now,
+    utc_now,
     get_local_utcoffset,
     make_tzaware,
     localized_dt_string,
@@ -19,11 +19,11 @@ class User(db.Model):
     """Модель пользователя для хранения учетных данных и других данных."""
 
     __tablename__ = "site_user"
-    
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.String(256), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
     password_hash = db.Column(db.String(100), nullable=False)
-    registered_on = db.Column(db.DateTime, default=uts_now)
+    registered_on = db.Column(db.DateTime, default=utc_now)
     admin = db.Column(db.Boolean, default=False)
     public_id = db.Column(db.String(36), unique=True, default=lambda: str(uuid4()))
 
@@ -31,28 +31,27 @@ class User(db.Model):
         return (
             f"<User email={self.email}, public_id={self.public_id}, admin={self.admin}>"
         )
-    
+
     @hybrid_property
     def registered_on_str(self):
-        """преобразует значение даты и времени, хранящееся в registered_on в форматированную строку."""
-        registered_on_uts = make_tzaware(
+        registered_on_utc = make_tzaware(
             self.registered_on, use_tz=timezone.utc, localize=False
         )
-        return localized_dt_string(registered_on_uts, use_tz=get_local_utcoffset())
+        return localized_dt_string(registered_on_utc, use_tz=get_local_utcoffset())
 
     @property
     def password(self):
-        raise AttributeError("пароль: поле только для записи")
-    
+        raise AttributeError("password: write-only field")
+
     @password.setter
     def password(self, password):
         log_rounds = current_app.config.get("BCRYPT_LOG_ROUNDS")
         hash_bytes = bcrypt.generate_password_hash(password, log_rounds)
-        self.pasword_hash = hash_bytes.decode("utf-8")
+        self.password_hash = hash_bytes.decode("utf-8")
 
-    def check_passsword(self, password):
+    def check_password(self, password):
         return bcrypt.check_password_hash(self.password_hash, password)
-    
+
     def encode_access_token(self):
         now = datetime.now(timezone.utc)
         token_age_h = current_app.config.get("TOKEN_EXPIRE_HOURS")
@@ -63,11 +62,11 @@ class User(db.Model):
         payload = dict(exp=expire, iat=now, sub=self.public_id, admin=self.admin)
         key = current_app.config.get("SECRET_KEY")
         return jwt.encode(payload, key, algorithm="HS256")
-    
+
     @classmethod
     def find_by_email(cls, email):
         return cls.query.filter_by(email=email).first()
-    
+
     @classmethod
     def find_by_public_id(cls, public_id):
         return cls.query.filter_by(public_id=public_id).first()
